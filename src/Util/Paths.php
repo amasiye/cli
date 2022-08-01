8f9f98d729fce5223b5229b7dcd779f3adf1f397
Andrew Masiye <andrew.masiye@liferaftinc.com>
2022-08-01 02:57:05 +0200

    feat: create TokenInspector.php

---
+++ src/Util/Paths.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Assegai\Cli\Util;
+
+final class Paths
+{
+  private function __construct()
+  {
+  }
+
+  /**
+   * @return string
+   */
+  public static function getWorkingDirectory(): string
+  {
+    return trim(shell_exec("pwd"));
+  }
+
+  public static function getAssegaiPath(): string
+  {
+    return trim(shell_exec('echo $ASSEGAI_PATH'));
+  }
+
+  public static function getBaseDirectory(): string
+  {
+    return dirname(__DIR__, 2);
+  }
+}
