f90475a7c6238af9ca198cd49b252a3fa8acd2fa
Andrew Masiye <andrew.masiye@liferaftinc.com>
2022-08-02 22:26:06 +0200

    refactor(util): add PHPDoc block comments to Paths.php

--- src/Util/Paths.php
+++ src/Util/Paths.php
@@ -2,6 +2,9 @@
 
 namespace Assegai\Cli\Util;
 
+/**
+ * Defines static methods for managing some useful paths.
+ */
 final class Paths
 {
   private function __construct()
@@ -16,18 +19,35 @@ final class Paths
     return trim(shell_exec("pwd"));
   }
 
+  /**
+   * @return string
+   */
   public static function getAssegaiPath(): string
   {
     return trim(shell_exec('echo $ASSEGAI_PATH'));
   }
 
+  /**
+   * @return string
+   */
   public static function getBaseDirectory(): string
   {
     return dirname(__DIR__, 2);
   }
 
+  /**
+   * @return string
+   */
   public static function getResourceDirectory(): string
   {
     return self::getBaseDirectory() . '/res';
+  }
+
+  /**
+   * @return string
+   */
+  public static function getAssegaiConfigPath(): string
+  {
+    return self::getWorkingDirectory() . '/assegai.json';
   }
 }
