06776b5ee39ef0ad355c6db9c93797df82c4b3d4
Andrew Masiye <andrew.masiye@liferaftinc.com>
2022-08-02 22:07:52 +0200

    feat(commands): create UpdateCommand.php

---
+++ src/Commands/UpdateCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Assegai\Cli\Commands;
+
+use Assegai\Cli\Attributes\Command;
+use Assegai\Cli\Core\AbstractCommand;
+use Assegai\Cli\Core\Console\Console;
+use Assegai\Cli\Enumerations\Color\Color;
+use Assegai\Cli\Enumerations\Color\TextStyle;
+use Assegai\Cli\Interfaces\IArgumentHost;
+
+#[Command(
+  name: 'update',
+  usage: 'update [options]',
+  shortName: 'u',
+  description: 'Updates your application and its dependencies. See https://update.assegai.ml/'
+)]
+class UpdateCommand extends AbstractCommand
+{
+
+  public function execute(IArgumentHost $context): int
+  {
+    Console::print(message: sprintf(
+      "%s%s▹▹▹▹▹%s Update in progress... ☕\n",
+      Color::LIGHT_BLUE,
+      TextStyle::BLINK->value,
+      Color::RESET
+    ));
+
+    shell_exec("composer update");
+
+    Console::print(message: "\n✔️ Update complete! \n");
+
+    return Command::SUCCESS;
+  }
+}
