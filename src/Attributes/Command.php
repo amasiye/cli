eb7349f13a70ad0185728d18f3f811e4ba1ee670
Andrew Masiye <andrew.masiye@liferaftinc.com>
2022-08-02 22:04:56 +0200

    refactor(attributes): add more attribute arguments to Command.php

--- src/Attributes/Command.php
+++ src/Attributes/Command.php
@@ -2,6 +2,8 @@
 
 namespace Assegai\Cli\Attributes;
 
+use Assegai\Cli\Core\CommandArgument;
+use Assegai\Cli\Core\CommandOption;
 use Attribute;
 
 #[Attribute(Attribute::TARGET_CLASS)]
@@ -10,12 +12,23 @@ class Command
   const SUCCESS = 0;
   const ERROR_DEFAULT = 1;
 
+  /**
+   * @param string $name
+   * @param string|null $usage
+   * @param string|null $shortName
+   * @param string $description
+   * @param string|null $longDescription
+   * @param CommandOption[] $options
+   * @param CommandArgument[] $arguments
+   */
   public final function __construct(
     public readonly string  $name,
-    public readonly string  $usage,
+    public readonly ?string  $usage = null,
     public readonly ?string $shortName = null,
     public readonly string  $description = '',
     public readonly ?string $longDescription = null,
+    public readonly array $options = [],
+    public readonly array $arguments = [],
   )
   {
   }
