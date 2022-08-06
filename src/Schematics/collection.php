bf152c290df5e337c96ffd127f787500ea75cd3b
Andrew Masiye <andrew.masiye@liferaftinc.com>
2022-08-06 02:05:25 +0200

    refactor(schematics): add dynamic schema loading

--- src/Schematics/collection.php
+++ src/Schematics/collection.php
@@ -1,10 +1,22 @@
 <?php
 
 use Assegai\Cli\Enumerations\SchematicType;
-use Assegai\Cli\Schematics\Module\ModuleSchematic;
+use Assegai\Cli\Util\Text;
+
+function classifySchemaName($name): string
+{
+  $output = Text::kebabToPascal($name);
+  return sprintf("%sSchematic", $output);
+}
+
+$schematicTypes = SchematicType::cases();
+$schema = [];
+
+foreach ($schematicTypes as $type)
+{
+  $schema[$type->value] = 'Assegai\Cli\Schematics\Module\\' . classifySchemaName($type->value);
+}
 
 return [
-  'schema' => [
-    SchematicType::MODULE->value => ModuleSchematic::class
-  ]
+  'schema' => $schema
 ];
