42dcfa9476fd0ff5071f4586706369b0270da3af
Andrew Masiye <andrew.masiye@liferaftinc.com>
2022-08-10 11:02:19 +0200

    feat(schematics): create resource schematic template files

---
+++ src/Schematics/Module/Files/__name@pascalize__Module.template.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Assegai\App\%== __name@pascalize__ ==%;
+
+use Assegai\App\Users\Guards\MethodGuard;
+use Assegai\Core\Attributes\Modules\Module;
+
+#[Module(
+  providers: [],
+  controllers: [],
+  imports: [],
+  exports: [],
+)]
+class %== __name@pascalize__ ==%Module {}
