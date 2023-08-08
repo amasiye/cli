21ba1b5b451630cefa6fd0ec6f78e6d174626b42
Andrew Masiye <andrew.masiye@liferaftinc.com>
2023-08-08 20:22:07 +0200

    refactor(util/logger): add default value for log methods

--- src/Util/Logger/Log.php
+++ src/Util/Logger/Log.php
@@ -6,17 +6,29 @@ use Monolog\Logger;
 use Psr\Log\LoggerInterface;
 use Psr\Log\LogLevel;
 
+/**
+ * Defines a logger for the CLI.
+ */
 class Log
 {
+  /** @var Log|null $instance */
   protected static ?Log $instance = null;
   /** @var Logger $logger  */
   protected LoggerInterface $logger;
 
+  /**
+   * Constructs the logger.
+   */
   private final function __construct()
   {
     $this->logger = new Logger('Assegai CLI');
   }
 
+  /**
+   * Gets the singleton instance of the logger.
+   *
+   * @return Log
+   */
   public static function getInstance(): self
   {
     if (!self::$instance)
@@ -27,22 +39,48 @@ class Log
     return self::$instance;
   }
 
+  /**
+   * Sets the logger to use.
+   *
+   * @param LoggerInterface $logger
+   */
   public function setLogger(LoggerInterface $logger): void
   {
     $this->logger = $logger;
   }
 
+  /**
+   * Logs a message at the info level.
+   *
+   * @param string $tag
+   * @param string $message
+   * @param array $context
+   */
   public function log(string $tag, string $message, array $context = []): void
   {
     $this->logger->log(level: LogLevel::NOTICE,message: "$tag: $message", context: $context);
   }
 
-  public function debug(string $tag, string $message, array $context): void
+  /**
+   * Logs a message at the debug level.
+   *
+   * @param string $tag
+   * @param string $message
+   * @param array $context
+   */
+  public function debug(string $tag, string $message, array $context = []): void
   {
     $this->logger->debug(message: "$tag: $message", context: $context);
   }
 
-  public function error(string $tag, string $message, array $context): void
+  /**
+   * Logs a message at the error level.
+   *
+   * @param string $tag
+   * @param string $message
+   * @param array $context
+   */
+  public function error(string $tag, string $message, array $context = []): void
   {
     $this->logger->error(message: "$tag: $message", context: $context);
   }
